3fb488443f2d8992ff021c4e41a44d0c372b128f
Petar <petar@bellabeat.com>
2017-07-26 10:50:55 +0200

    init commit

---
+++ app/src/main/java/com/petarmarijanovic/rxactivityresult/Main2Activity.kt
@@ -0,0 +1,21 @@
+package com.petarmarijanovic.rxactivityresult
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class Main2Activity : AppCompatActivity() {
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main2)
+    
+    findViewById(R.id.ok).setOnClickListener { finish(Activity.RESULT_OK) }
+    findViewById(R.id.cancel).setOnClickListener { finish(Activity.RESULT_CANCELED) }
+  }
+  
+  private fun finish(result: Int) {
+    setResult(result)
+    finish()
+  }
+}
---
+++ app/src/main/java/com/petarmarijanovic/rxactivityresult/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.petarmarijanovic.rxactivityresult
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    
+    findViewById(R.id.enable).setOnClickListener { enable() }
+  }
+  
+  private fun enable() {
+    RxActivityResult(this).observe(Intent(this, Main2Activity::class.java),
+                                   123456).subscribe({ Log.d("Petarr", "success " + it) },
+                                                     { Log.e("Petarr", "fail " + it.message) })
+    
+  }
+}
