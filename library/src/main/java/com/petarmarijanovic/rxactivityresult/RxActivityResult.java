18d16f2aecacdc4ac983cd00553718b59fb45653
Oleksii Trotsenko <oleksii.trotsenko@adeya.ch>
2019-09-26 14:44:24 +0200

    Added ability to create explicit intent without providing context
    RxActivityResult.newExplicitIntent(OtherActivity.class) creates new explicit intent.
    Thus it could be used in any class (not just in Activity or Fragment).
    Latter it can be passed to RxActivityResult.start() in the similar way how
     you can pass any other implicit intent.

--- library/src/main/java/com/petarmarijanovic/rxactivityresult/RxActivityResult.java
+++ library/src/main/java/com/petarmarijanovic/rxactivityresult/RxActivityResult.java
@@ -1,6 +1,7 @@
 package com.petarmarijanovic.rxactivityresult;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
@@ -40,5 +41,26 @@ public class RxActivityResult {
   /** TODO: Write JavaDoc. */
   public Single<ActivityResult> start(final PendingIntent pendingIntent) {
     return fragment.start(pendingIntent);
+  }
+
+  /**
+   * Create an intent for a specific component.  All other fields (action, data,
+   * type, class) are null, though they can be modified later with explicit
+   * calls.  This provides a convenient way to create an intent that is
+   * intended to execute a hard-coded class name, rather than relying on the
+   * system to find an appropriate class for you; see {@link Intent#setComponent}
+   * for more information on the repercussions of this.
+   *
+   * NOTE: Application context is used as package context.
+   *
+   * @param cls The component class that is to be used for the intent.
+   *
+   * @see Intent#setClass
+   * @see Intent#setComponent
+   * @see Intent#Intent(android.content.Context, java.lang.Class)
+   */
+  public Intent newExplicitIntent(Class<?> cls) {
+    Context applicationContext = fragment.getContext().getApplicationContext();
+    return new Intent(applicationContext, cls);
   }
 }
